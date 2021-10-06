72712c0e01314ade78f1623af6b61d4458ae18d9
Niporka <pavelniporka@gmail.com>
2021-10-06 16:57:23 +0300

    Initial commit

---
+++ src/test/java/com/example/registrtionloginsringboot/RegistrtionLoginSringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrtionloginsringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrtionLoginSringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
